c165a1f33cbd48a31d4978a09802f68181ce514d
mwhorse46 <needmefordev@gmail.com>
2020-11-26 12:49:37 +0700

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,15 +9,23 @@
         <div class="d-flex justify-content-center m-3">
             <h4>Login</h4>
         </div>
-        <div class="">
+        <div class="m-4 px-3">
             <form>
                 <div class="form-group row">
-                    <label for="email" class="col-sm-4 col-form-label">Email Address</label>
-                    <input type="email" class="col-sm-8 form-control" id="txtEmail">
+                    <label for="txtEmail" class="col-sm-4 col-form-label">Email Address</label>
+                    <input type="email" class="col-sm-7 form-control mx-3" id="txtEmail">
                 </div>
                 <div class="form-group row">
-                    <label for="password" class="col-sm-4 col-form-label">Password</label>
-                    <input type="password" class="col-sm-8 form-control" id="txtPassword">
+                    <label for="txtPassword" class="col-sm-4 col-form-label">Password</label>
+                    <input type="password" class="col-sm-7 form-control mx-3" id="txtPassword">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="form-check-input" id="rememberMe">
+                    <label for="rememberMe" class="form-check-label">Remember Me</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+                <div class="pt-1">
+                    <a href="#">Forgot Password?</a>
                 </div>
             </form>
         </div>
